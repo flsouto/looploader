ed7227e7214913544a446692a851954c3d2be90c
flsouto <fabiolimasouto@gmail.com>
2023-07-15 20:25:50 -0300

    updates

--- example.config.php
+++ example.config.php
@@ -3,7 +3,7 @@
 return [
     'smp_path' => __DIR__."/../sampler/FlSouto/Sampler.php",
     'queue_path' => __DIR__."/queue/*.mp3",
-    'cookies_path' => '~/Downloads/cookies',
+    'cookies_path' => '/home/fabio/Downloads/cookies',
     'description' => ['Enjoy'],
     'genre_id' => ['38','9','35','2','36'],
     'cat_id' => ['1','5']
--- upload.php
+++ upload.php
@@ -1,9 +1,8 @@
 <?php
 use FlSouto\Sampler;
-$conf = require(__DIR__."/config.php");
-require_once($conf['smp_path']);
+require_once(__DIR__."/utils.php");
 
-$uploaded = array_flip(file(__DIR__."/uploaded.log"));
+$uploaded = array_flip(array_map('trim',file(__DIR__."/uploaded.log")));
 $file = '';
 $hash = '';
 
@@ -20,13 +19,12 @@ if(!$file){
     die("All files from queue uploaded\n");
 }
 
-$s = new Sampler($file);
-$bpm = 'XXX'; // todo resize and calc BPM
+$s = sampler($file);
+$bpm = fixBPM($s);
 
 if(strstr($file,'.mp3')){
     $s->save($file = "/dev/shm/$hash.wav");
 }
-
 $ch = curl_init("https://www.looperman.com/loops/admin");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_COOKIEFILE, $conf['cookies_path']);
@@ -43,7 +41,7 @@ $cookies = "loop_csrfc=".$csrf;
 
 $fields['csrftoken'] = $csrf;
 $fields['loop_title'] = $hash;
-$fields['loop_desc'] = pick($conf['desc']);
+$fields['loop_desc'] = pick($conf['description']);
 $fields['loop_wav'] = new CurlFile($file, 'audio/wav');
 $fields['loop_cat_id'] = pick($conf['cat_id']);
 $fields['loop_genre_id'] = pick($conf['genre_id']);
@@ -52,7 +50,6 @@ $fields['loop_tempo'] = $bpm;
 $fields['loop_key'] = '';
 $fields['loop_disclaimer'] = '1';
 $fields['submit'] = 'Insert';
-
 $ch = curl_init("https://www.looperman.com/loops/admin");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
@@ -62,10 +59,11 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: multipart/form-data']);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 curl_setopt($ch, CURLOPT_COOKIE, $cookies);
-curl_setopt($ch, CURLOPT_COOKIEFILE, $conf['smp_path']);
+curl_setopt($ch, CURLOPT_COOKIEFILE, $conf['cookies_path']);
 //curl_setopt($ch, CURLOPT_VERBOSE, true);
 
 $out = curl_exec($ch);
+file_put_contents(__DIR__."/output.html", $out);
 
 file_put_contents(__DIR__."/uploaded.log","$hash\n",FILE_APPEND);
-shell_exec("git add uploaded.log; git commit -m 'uploaded'; git push origin main");
+shell_exec("git add uploaded.log; git commit -m 'uploaded'; git push origin master");
