c848acc2d34d75b3e634b2d24f785b83eac7942c
flsouto <fabiolimasouto@gmail.com>
2023-08-05 14:15:38 -0300

    updates

--- example.config.php
+++ example.config.php
@@ -2,8 +2,11 @@
 
 return [
     'smp_path' => __DIR__."/../sampler/FlSouto/Sampler.php",
-    'queue_path' => __DIR__."/queue/*.mp3",
-    'cookies_path' => '/home/fabio/Downloads/cookies',
+    'queue_path' => __DIR__."/queue/*.wav",
+    'backup_paths' => [
+        '192.168.0.55:~/Documents/looploader-backups/'
+    ],
+    'cookies_path' => '/home/fabio/Downloads/cookies.txt',
     'description' => ['Enjoy'],
     'genre_id' => ['38','9','35','2','36'],
     'cat_id' => ['1','5']
